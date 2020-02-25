3c35aa751ab83182c45a2c4d7e3b94a25be871e8
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-02-24 20:41:43 -0500

    package.json

---
+++ server/game/quoteCards.js
@@ -0,0 +1,6 @@
+const quoteCards = [
+    "This class is awesome",
+    "Come to shabbat 200"
+];
+
+module.exports= quoteCards;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const quoteCards = require('./game/quoteCards');
 const app = express();
 const port = 3000;
 
 app
-.get('/', (req, res) => res.send('this class is awsome!'));
+    .get('/', (req, res) => res.send('this class is awsome!') )
+    .get('/game/quoteCards', (req, res) => res.send(quoteCards) );
+
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}!`));
 
