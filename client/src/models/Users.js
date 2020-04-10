e3273de54455c90499b564c0c57b127e63f714bd
pavani <gorlagup1@hawkmail.newpaltz.edu>
2020-04-09 21:06:20 -0400

    --- Auto-Git Commit ---

--- client/src/models/Users.js
+++ client/src/models/Users.js
@@ -6,16 +6,17 @@ const Users = [
     
 ];
 
- let CurrentUser = null;
- function Login(email, password) {
+ let Users = {
+    CurrentUser:null,
+    Login(email,password){
+ 
 
     const user = Users.find(x => x.Email == email);
     if(!user) throw Error('User not found');
     if(user.Password != password) throw Error('Wrong Password');
 
-    return  this.CurrentUser = user;
+    return this.CurrentUser = user;
 }
-
-export default {
-    CurrentUser, Login
-}
+ };
+ 
+export default Users;
